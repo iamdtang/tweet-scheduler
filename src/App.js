b5eab6f6bb0d23189e59acdfe79c92a47a4e6504
David Tang <dtang859@gmail.com>
2018-04-28 12:08:03 -0700

    adds tailwind

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweets: [
+        {}
+      ]
+    };
+  }
+
+  addTweet = (tweet) => {
+    this.setState({
+      tweets: this.state.tweets.concat(tweet)
+    });
+  }
+
+  schedule = (e) => {
+    e.preventDefault();
+  }
+
   render() {
+    let tweetRows = this.state.tweets.map((tweet, i) => {
+      let id = `tweet-${i}`;
+      return (
+        <div key={i}>
+          <label htmlFor={id}>Tweet</label>
+          <textarea id={id}></textarea>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Tweet Scheduler</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <form onSubmit={this.schedule}>
+            {tweetRows}
+            <button type="button" className="btn btn-blue" onClick={this.addTweet}>Add another tweet</button>
+
+            <div>
+              <label htmlFor="interval">Interval</label>
+              <input type="number" id="interval" />
+            </div>
+            <button type="submit" className="btn btn-blue">Schedule</button>
+          </form>
+        </main>
+        <footer>
+
+        </footer>
       </div>
     );
   }
